41493d062e606250f49f49e9a91db0a06003101c
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-02 23:15:57 +0800

    Update Class.cpp

--- Class.cpp
+++ Class.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <vector>
-
 using std::cout;
 
-
-class User//default class is public
+class User // default class is private
 {
 	std::string status = "Passed";
 
@@ -19,7 +17,7 @@ public:
 
 int add_user_if_not_exist(std::vector<User>& users, User user)
 {
-	for (int i = 0; i < users.size(); i++)//user given 3..index 0,1,2
+	for (int i = 0; i < users.size(); i++) // user given 3..index 0,1,2
 	{
 		if (users[i].first_name == user.first_name &&
 			users[i].last_name == user.last_name)
@@ -33,28 +31,28 @@ int add_user_if_not_exist(std::vector<User>& users, User user)
 
 int main()
 {
-	std::vector<User> users;//users is the name of the vector
+	std::vector<User> users; // users is the name of the vector
 
 	User user1, user2, user3;
 
-	user1.first_name = "Aminul";//index 0
-	user1.last_name = "Aiman";
+	user1.first_name = "Yagami"; // index 0
+	user1.last_name  = "Light";
 
-	user2.first_name = "Karim";//index 1
-	user2.last_name = "Ahmad";
+	user2.first_name = "Kyoko"; // index 1
+	user2.last_name  = "Miyamura";
 
-	user3.first_name = "Loki";//index 2
-	user3.last_name = "Odinson";
+	user3.first_name = "Loki"; // index 2
+	user3.last_name  = "Odinson";
 
-	users.push_back(user1);//this code give user1,user2,user3..
+	users.push_back(user1);//this code give user1,user2,user3
 	users.push_back(user2);//its index.
 	users.push_back(user3);
 
 	User user;
 
-	user.first_name = "Thor";//if new user detected 
-	user.last_name = "Odinson";//it will be taken new index
+	user.first_name = "Thor";   // If new user detected 
+	user.last_name = "Odinson"; // New index created
 
-	std::cout << add_user_if_not_exist(users, user);//index will be displayed
+	std::cout << add_user_if_not_exist(users, user); // New index will be displayed
 	return 0;
 }
