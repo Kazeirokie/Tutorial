a69c62ff46881c062f72e46bd8487ee2e2c61af0
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-02 23:59:34 +0800

    Update Constructor.cpp

--- Constructor.cpp
+++ Constructor.cpp
@@ -1,62 +1,55 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
-using std::cout;
-
-
-class User//default class is private
+class User // default class is private
 {
 	std::string _status = "Passed";
 
 public:
 	std::string _first_name;
 	std::string _last_name;
-	std::string get_status()//this is getter..get info from private class
+	std::string get_status() // This is getter..get info from private class
 	{
 		return _status;
 	}
 
-
-	void set_status(std::string status)//this is setter..see if input exist
+	void set_status(std::string status) // This is setter..see if input exist
 	{
-		if (status == "Passed" || status == "Failed")
+		if (status == "Aplus" || status == "Asolid")
 		{
-			this->_status = status;
+			cout<<"Congratulations!!!"<<endl;
 		}
 		else
 		{
-			std::cout << "You are not registered";
+			std::cout << "Better Next Time.";
 		}
 	}
 
-
-	//constructor
+	// Constructor
 	User(std::string first_name, std::string last_name, std::string status)
 	{
-		this->_first_name = first_name;//take the var from class
+		this->_first_name = first_name; // Variable from class
 		this->_last_name = last_name;
 		this->_status = status;
 	}
-	~User()//destructor is to end constructor
+	~User() // Destructor is to end constructor
 	{
 		std::cout <<"\n" << "Destructor";
 	}
-
 };
-
-
 
 int main()
 {
-	User user("Aminul", "Aiman", "Passed");//refer back to constructor
-	std::cout << user._first_name;//first_name refer to ..constructor..refer to class
+	User user("Tori", "Pota", "Passed"); // Refer back to Constructor
+	std::cout << user._first_name;       // first_name refer to ..Constructor..refer to class
 
-	//user.status can't be accessed since it's private//
-	//can accessed by using getter//'
+	// _status can't be accessed since it's private
+	// can accessed by using getter
 
-	std::cout << "\n" << user.get_status() << "\n";//from getter..get status from class
+	std::cout << "\n" << user.get_status() << "\n"; // from getter..get status from class
 
-	user.set_status("No");//from setter..check if the status right or not
+	user.set_status("Aplus"); // from setter..check if the status right or not
 
 }
-//destructor will be executed everytime constructor ended
+// Destructor will be executed everytime constructor ended
