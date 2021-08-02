e00435a40b9e782e5c7c00744f889aabf9c7e299
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 00:03:32 +0800

    Update Default_argument.cpp

--- Default_argument.cpp
+++ Default_argument.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
 double pow(double base, int pow = 2)
+// If pow is 2 then loop will run 2 times
 {
 	int total = 1;
 	for (int i = 0;i < pow;i++)
-		//if pow is 2 then loop will run 2 times
 	{
-		total *= base;//(1x3)+(1x3)
+		total *= base; // (1x3)+(1x3)
 	}
 	return total;
 }
+
 int main()
 {
 	std::cout << pow(3, 3) << "\n";
