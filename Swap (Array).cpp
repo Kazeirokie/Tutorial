79d51cc6a4b86352e4b8bc6590d5fad16afd8297
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 12:47:10 +0800

    Update Swap (Array).cpp

--- Swap (Array).cpp
+++ Swap (Array).cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-
 template <typename X>
+
 void swap(X a[], X b[], int size)
 {
 	for (int i = 0;i < size;i++)
@@ -11,15 +11,16 @@ void swap(X a[], X b[], int size)
 		b[i] = temp;
 	}
 }
+
 int main()
 {
-	const int SIZE = 6;//same like nines and ones size
-	int nines[] = { 9,9,9,9,9,9 };
-	int ones[]  = { 1,1,1,1,1,1 };
+	int SIZE = 6;                   // SIZE to activate void swap
+	int nines[] = { 9,9,9,9,9,9 };  // Array to be swapped
+	int ones[]  = { 1,1,1,1,1,1 };  // Array to be swapped
 	
-	for (int i = 0; i<6 ;i++)
+	for (int i = 0; i < 6 ;i++)
 	{
-		std::cout << nines[i] << " " << ones[i] << "\t";//execute 9 and 1 repeatedly
+		std::cout << nines[i] << " " << ones[i] << "\t"; // Execute 9 and 1 repeatedly
 	}
 	std::cout << "\n\n";
 
@@ -27,7 +28,7 @@ int main()
 
 	for (int i = 0; i < 6; i++)
 	{
-		std::cout << nines[i] << " " << ones[i] << "\t";//swap
+		std::cout << nines[i] << " " << ones[i] << "\t"; //Swap
 	}
 	std::cout << "\n\n";
 }
