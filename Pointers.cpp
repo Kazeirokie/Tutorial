086cb2686639e2ab59e3aeb1b287610cc0cbc484
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 13:46:02 +0800

    Update Pointers.cpp

--- Pointers.cpp
+++ Pointers.cpp
@@ -4,20 +4,15 @@ using namespace std;
 int main()
 {
 	int x = 5;
-	cout << &x<<"\n\n";//execute x address
+	cout <<"x address is "<< &x<<"\n\n"; // Execute x address
 
-	int* y = &x;//give back the real value of x
+	int* y = &x;        // Give back the real value of x
 
 	cout << *y <<" same as "<< x << "\n\n";
-
 
 	if (x == *y)
 	{
 		cout << "True";
 	}
-	else 
-	{
-		cout << "False" << endl;
-	}
-
+	else cout << "False" << endl;
 }
