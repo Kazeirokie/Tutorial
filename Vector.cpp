231af21e33629ed7a982a68427f908173ad4510e
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 11:43:40 +0800

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
 #include<vector>
+using namespace std;
 
 
 void print_vector(std::vector<int> data)
 {
-	for (int i = 0; i < data.size(); i++)
-		//data size is 3 according to 1,2,3
-		//i is 0,1,2 and the code will execute 1,2,3
-	{
-		std::cout << data[i] << "\t";
+	for (int i = 0; i < data.size(); i++)  // data size = 3 according to 1,2,3   	                                  
+	{                                      // i = 0,1,2 and the code will execute 1,2,3
+		cout << data[i] << "\t";
 	}
-	std::cout << "\n" << std::endl;//space downward by one
+	cout << "\n" << endl;
 }
+
 int main()
 {
-	/* //several useful func
-	std::vector<int> data = {1,2,3};//same like array
-	data.push_back(21);//is add 21 after 3
-	std::cout << data[data.size() - 1] << std::endl;//data.size is 4 after added 21
-	data.pop_back();//buang satu dr blakang
-	std::cout << data.size() << std::endl;
+	/* 
+	std::vector<int> data = {1,2,3};          // Vector 
+	data.push_back(21);//is add 21 after 3    // push.back add one data
+	cout << data[data.size() - 1] << endl;    // data.size is 4 after added 21
+	data.pop_back();                          // pop.back erase one data
+	cout << data.size() << endl;
 	*/
 
-	//work with void func
+	// Work with print_vector
 	std::vector<int> data = { 1,2,3 };
 	print_vector(data);
 }
-//search geeksforgeeks vector in C++ for more...
+//search geeksforgeeks vector in C++ for more info.
