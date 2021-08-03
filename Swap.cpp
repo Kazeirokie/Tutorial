f8d398840093bc29e0d5f75303b01863b5085044
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 13:53:10 +0800

    Update Swap.cpp

--- Swap.cpp
+++ Swap.cpp
@@ -6,12 +6,27 @@ void swap(int &a, int &b) //& sign check for all preferences..
 	int temp = a;  // "a" data copied into temp
 	a = b;       // "b" data copied into "a"
 	b = temp;    // temp data copied into "b"
+	cout << "a: " << a << "\n" << "b: " << b <<"\n" << std::endl;
 }
 
+
+void swap(std::string a, std::string b)
+{	
+	std::string temp =a;
+	a = b;
+	b = temp;
+	cout << "a: " << a << "\n" << "b: " << b <<"\n" << std::endl;
+}
+	
+	
 int main()
 {
 	int a = 10;
 	int b = 20;
 	swap(a, b);
-	cout << "a: " <<a<< "\n" << "b: " <<b<<"\n" << std::endl;
+	
+	
+	std::string last_name = "Omi";
+	std::string first_name = "Tanuki";
+	swap(last_name,first_name );
 }
