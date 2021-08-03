1c5a877d65817947c17f07609170afd9b7c2f92f
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 13:07:08 +0800

    Update Simple Switch.cpp

--- Simple Switch.cpp
+++ Simple Switch.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
-using std::cout;
-using std::cin;
+using namespace std;
 
 int main()
 {
-	//enum class Season { summer, spring, fall, winter };
-	//Season now = Season::spring;
+	enum class Season { summer, spring, fall, winter };
+	Season now = Season::spring;
 
-	/*switch (now)
+	switch (now)
 	{
 	case Season::summer:
-		cout << "Wow! It's very hot outside.";
+		cout << "Wow! It's very hot outside.\n\n";
 		break;
 	case Season::spring:
-		cout << "Flowers are blooming.";
+		cout << "Flowers are blooming.\n\n";
 		break;
 	case Season::fall:
-		cout << "Look! It's browny outside";
+		cout << "Look! It's browny outside\n\n";
 		break;
 	case Season::winter:
-		cout << "Let's build a snowman!";
+		cout << "Let's build a snowman!\n\n";
 		break;
-	}*/
+	default:
+		break;
+	}
 
 	int age;
 	cout << "What is your age? \n";
 	cin >> age;
 	switch (age)
 	{
-	case 10:
+	case 10: // 10 & 11 will execute same output
 	case 11:
 		cout << "You're too young.";
 		break;
@@ -41,9 +42,7 @@ int main()
 	default:
 		cout << "Put a suitable age only! \n\n";
 		main();
-
 	}
-
 }
 
-//switch limited to integral only
+// switch limited to int only
