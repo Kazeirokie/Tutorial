595d3d701aaa08a4e26004642ee85b359a264fd9
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 12:50:50 +0800

    Update Struct.cpp

--- Struct.cpp
+++ Struct.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
 
-using std::cout;
+using namespace std;
 
-
-struct User//default struct is public
+struct User // default struct is public
 {
 	std::string first_name;
 	std::string last_name;
@@ -13,17 +12,16 @@ struct User//default struct is public
 		return status;
 	}
 	private:
-			std::string status = "Passed";
+	std::string status = "Passed";
 };
 
 int main()
 {
 	User user_1;
-	user_1.first_name = "Aminul";
-	user_1.last_name = "Aiman";
+	user_1.first_name = "Tom";
+	user_1.last_name = "Anderson";
 
-	cout << "First name: " << user_1.first_name << "\n" << "Last name: " 
-		 << user_1.last_name << "\n" << "Status : " << user_1.get_status();
+	cout << "First name: " << user_1.first_name << "\n" 
+	<< "Last name: " << user_1.last_name << "\n" 
+	<< "Status : " << user_1.get_status() << endl;
 }
-
-
