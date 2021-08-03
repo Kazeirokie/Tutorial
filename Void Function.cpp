2649a6b43f3ab6a95e091ef8b42d9bd032fbdbe2
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 11:36:12 +0800

    Update Void Function.cpp

--- Void Function.cpp
+++ Void Function.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
+using namespace std;
 
 double power(double base, int exponent)
 {
@@ -13,29 +11,28 @@ double power(double base, int exponent)
 	return result;
 }
 
-
 void print_pow(double base, int exponent) 
 {
 	double myPower = power(base, exponent);
 	cout << base << " raised to the " << exponent <<" power is " << myPower << endl;
 }
 
-
 int main()
 {
 	double base;
 	int exponent;
-	//cout << "Type the base : ";
-	//cin >> base;
-	//cout << "Type the power : ";
-	//cin >> exponent;
-	//print_pow(base, exponent); //read the comment first...example ...
-	print_pow(10, 3); //We can get as many data as we want..yay
-	print_pow(9, 2);
-	print_pow(8, 10);
+	
+	/*
+	cout << "Type the base : ";    // Work with print_pow function
+	cin >> base;                   // Work with print_pow function
+	cout << "Type the power : ";   // Work with print_pow function
+	cin >> exponent;               // Work with print_pow function
+	print_pow(base, exponent);     // Work with print_pow function
+	*/
+	
+	print_pow(10, 3); // Work with power function
+	print_pow(9, 2);  // Work with power function
+	print_pow(8, 10); // Work with power function
 }
-
-//BENEFITS :-
-//void function functions as the constant.
-//dont need to 'repeat the code' or 'delete and rewrite' to get various data.
-//Uncomment the cout, cin and first print_pow to see the effect.
+// I had separated the output
+// Uncomment the commented code to see other result
