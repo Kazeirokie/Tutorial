18d052feb6400a6e16c5b26599999924c6c6b317
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 13:54:51 +0800

    Update Multi-dimentional array - Nested vector.cpp

--- Multi-dimentional array - Nested vector.cpp
+++ Multi-dimentional array - Nested vector.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	std::vector<std::vector<int>> grades =
+	std::vector<std::vector<int>> grades 
 	 { 
 		{1,2,3},   // column 1 
 		{4,5,6},   // column 2
@@ -12,12 +12,11 @@ int main()
 
 	for (int r=0;r<3;r++)//r represent row
 	{
-		for (int c=0;c<1;c++)//c represent column
+		for (int c=0;c<3;c++)//c represent column
 		{
 			std::cout << grades[r][c] << "\t";
 		}
 		std::cout << "\n";
-
 	}
 }
-//chance r<1 / c<3 or r<3 / c<1 to see the effect
+// change r<1 / c<3 or r<3 / c<1 to see the effect
