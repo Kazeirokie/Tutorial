d40bc2815cfaba37a9c14adf7a98147c7e32f7c1
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 12:23:44 +0800

    Update Swap.cpp

--- Swap.cpp
+++ Swap.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
-using std::cout;
+using namespace std;
 
-template <typename N>//template function
-
-void swap(N &a, N &b)//& sign check for all preferences..
+void swap(int &a, int &b) //& sign check for all preferences..
 {
-	N temp = a;//"a" need to be new var in order to swap with b
-	a = b;
-	b = temp;
-	
-		
+	int temp = a;  // "a" data copied into temp
+	a = b;       // "b" data copied into "a"
+	b = temp;    // temp data copied into "b"
 }
+
 int main()
 {
 	int a = 10;
 	int b = 20;
 	swap(a, b);
 	cout << "a: " <<a<< "\n" << "b: " <<b<<"\n" << std::endl;
-
-	std::string first_name = "Aminul";
-	std::string last_name = "Aiman";
-	swap(first_name, last_name);
-	cout << "First name: " << first_name << "\n" << "Last name: " << last_name << std::endl;
 }
