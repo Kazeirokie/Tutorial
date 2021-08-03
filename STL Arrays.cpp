9358356942d0829a4b513e294a49c84ab85333d3
Kazeirokie <82706431+Kazeirokie@users.noreply.github.com>
2021-08-03 13:11:12 +0800

    Update STL Arrays.cpp

--- STL Arrays.cpp
+++ STL Arrays.cpp
@@ -2,16 +2,16 @@
 #include <array>
 #include <vector>
 
-void print_STL_array(std::array<int, 20> data)
+void print_STL_array(std::array<int, 100> data) // 100 is limit in array
 {
-	for (int i =0; i < data.size();i++)
-		//data.size is 20..i is 19..index start from 0
+	for (int i =0; i < data.size();i++) // data.size is 20..i is 19..index start from 0
 	{
 		std::cout << data[i] << "\t";
 	}
 }
+
 int main()
 {
-	std::array<int, 20> data = { 1,2,3 };
-	print_STL_array(data);
+	std::array<int, 20> data = { 1,2,3 }; // 20 data inserted in array
+	print_STL_array(data);                // 1,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 }
